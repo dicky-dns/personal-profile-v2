206c74c5dac511e3749f012594a1fdfca93f55b4
Dicky Dwi Nur Setyo <dickydns1@gmail.com>
2026-01-15 11:05:52 +0700

    style: update layout and spacing in Hero component and globals.css for improved responsiveness

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -4,44 +4,44 @@ export default function Hero() {
     return (
         <>
             <div className="hero">
-            <div className="container">
-                <div className="row">
-                    <div className="col-12">
-                        <div className="hero-image-desktop">
-                            {[
-                                ["image-d", "hero-desktop-d.png"],
-                                ["image-i", "hero-desktop-i.png"],
-                                ["image-c", "hero-desktop-c.png"],
-                                ["image-k", "hero-desktop-k.png"],
-                                ["image-y", "hero-desktop-y.png"],
-                                ["image-d2", "hero-desktop-d2.png"],
-                                ["image-n", "hero-desktop-n.png"],
-                                ["image-s", "hero-desktop-s.png"],
-                                ["image-icon", "hero-desktop-icon.png"],
-                                ].map(([cls, src]) => (
-                                <img key={cls} className={cls} src={"/images/" + src} alt="Logo" />
-                            ))}
-                        </div>
+                <div className="container">
+                    <div className="row">
+                        <div className="col-12">
+                            <div className="hero-image-desktop">
+                                {[
+                                    ["image-d", "hero-desktop-d.png"],
+                                    ["image-i", "hero-desktop-i.png"],
+                                    ["image-c", "hero-desktop-c.png"],
+                                    ["image-k", "hero-desktop-k.png"],
+                                    ["image-y", "hero-desktop-y.png"],
+                                    ["image-d2", "hero-desktop-d2.png"],
+                                    ["image-n", "hero-desktop-n.png"],
+                                    ["image-s", "hero-desktop-s.png"],
+                                    ["image-icon", "hero-desktop-icon.png"],
+                                    ].map(([cls, src]) => (
+                                    <img key={cls} className={cls} src={"/images/" + src} alt="Logo" />
+                                ))}
+                            </div>
 
-                        <div className="hero-image-mobile">
-                            {[
-                                ["image-d", "hero-mobile-d.png"],
-                                ["image-i", "hero-mobile-i.png"],
-                                ["image-c", "hero-mobile-c.png"],
-                                ["image-k", "hero-mobile-k.png"],
-                                ["image-y", "hero-mobile-y.png"],
-                                ["image-d2", "hero-mobile-d2.png"],
-                                ["image-n", "hero-mobile-n.png"],
-                                ["image-s", "hero-mobile-s.png"],
-                                ["image-icon", "hero-mobile-icon.png"],
-                            ]
-                            .map(([cls, src]) => (
-                                <img key={cls} className={cls} src={"/images/" + src} alt="Logo" />
-                            ))}
+                            <div className="hero-image-mobile">
+                                {[
+                                    ["image-d", "hero-mobile-d.png"],
+                                    ["image-i", "hero-mobile-i.png"],
+                                    ["image-c", "hero-mobile-c.png"],
+                                    ["image-k", "hero-mobile-k.png"],
+                                    ["image-y", "hero-mobile-y.png"],
+                                    ["image-d2", "hero-mobile-d2.png"],
+                                    ["image-n", "hero-mobile-n.png"],
+                                    ["image-s", "hero-mobile-s.png"],
+                                    ["image-icon", "hero-mobile-icon.png"],
+                                ]
+                                .map(([cls, src]) => (
+                                    <img key={cls} className={cls} src={"/images/" + src} alt="Logo" />
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
             </div>
 
             <div className="hero-divider"></div>
