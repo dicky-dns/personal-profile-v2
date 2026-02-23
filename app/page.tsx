6b5f6a021f3c76d8c1aece8de46105994b0ca4d5
Dicky Dwi Nur Setyo <dickydns1@gmail.com>
2026-02-23 12:48:52 +0700

    fix: remove unused imports from page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-import Link from 'next/link'
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skill from "./components/Skill";
